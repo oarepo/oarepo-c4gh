57dd895d258988ea009e2aeb8c811faf8e42b411
Dominik Pantůček <dominik.pantucek@trustica.cz>
2025-11-08 10:00:09 +0100

    Linear tests and documentation.
    - finish new Crypt4GH.open documentation
    - test thoroughly the Crypt4GHRawIO implementation
    - add some low-level tests (finished, short read)

--- oarepo_c4gh/crypt4gh/crypt4gh.py
+++ oarepo_c4gh/crypt4gh/crypt4gh.py
@@ -9,11 +9,33 @@ from io import BufferedReader, TextIOWrapper
 
 
 class Crypt4GH(Stream4GH):
-    """This class differs only in its name from the underlying
-    Stream4GH."""
+    """This class provides the user-facing API for the Stream4GH
+    functionality, adding the `open()` method for io-like interface.
+
+    """
 
     def open(self, mode: str = None, encoding: str = None) -> Crypt4GHRawIO:
-        """Use ... TextIOWrapper, BufferedIOWrapper"""
+        """Creates a Crypt4GHRawIO wrapper around self and returns
+        appropriate text or binary reader based on the mode and
+        encoding arguments.
+
+        Specify 'r' for explicit read mode - it is on by default.
+
+        Speficy 't' for explicit text mode - it is on by default.
+
+        Specify 'b' for binary mode.
+
+        For text mode (which is on by default) use `encoding` to
+        specify the text encoding wanted. If `None` the
+        `locale.getencoding()` is used.
+
+        Parameters:
+            mode: can contain 'r', 't' and 'b' characters.
+
+        Returns:
+            BufferedReader or TextIOWrapper based on the mode.
+
+        """
         mode_read = True
         mode_text = True
         if mode is not None:
@@ -25,7 +47,7 @@ class Crypt4GH(Stream4GH):
                 elif ch == "b":
                     mode_text = False
                 else:
-                    raise "error"
+                    raise OSError
         raw = Crypt4GHRawIO(self)
         buf = BufferedReader(raw)
         if mode_text:
--- tests/test_linear.py
+++ tests/test_linear.py
@@ -3,19 +3,70 @@ from _test_data import (
     alice_sec_bstr,
     alice_sec_password,
     hello_world_encrypted,
+    hello_world_corrupted,
 )
 from oarepo_c4gh import Crypt4GH, C4GHKey
 import io
+from oarepo_c4gh.crypt4gh.rawio import Crypt4GHRawIO
 
 
 class TestSimpleLinear(unittest.TestCase):
 
-    def test_byte_by_byte(self):
+    def test_byte_read(self):
         akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
         crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
         f = crypt4gh.open("b")
         b0 = f.read(1)
         assert b0 == b"H"
+
+    def test_explicit_open_r(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
+        f = crypt4gh.open("rb")
+        b0 = f.read(1)
+        assert b0 == b"H"
+
+    def test_char_read(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
+        f = crypt4gh.open("bt")
+        c0 = f.read(1)
+        assert c0 == "H"
+
+    def test_error_open_arg(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
+        self.assertRaises(OSError, lambda: crypt4gh.open("rbz"))
+
+    def test_not_writable(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
+        raw = Crypt4GHRawIO(crypt4gh)
+        assert raw.writable() == False
+
+    def test_finished(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
+        raw = Crypt4GHRawIO(crypt4gh)
+        b = bytearray(100)
+        nread1 = raw.readinto(b)
+        assert nread1 > 0
+        nread2 = raw.readinto(b)
+        assert nread2 == 0
+
+    def test_corrupted_block(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_corrupted), akey)
+        f = crypt4gh.open()
+        self.assertRaises(OSError, lambda: f.readline())
+
+    def test_small_read(self):
+        akey = C4GHKey.from_bytes(alice_sec_bstr, lambda: alice_sec_password)
+        crypt4gh = Crypt4GH(io.BytesIO(hello_world_encrypted), akey)
+        raw = Crypt4GHRawIO(crypt4gh)
+        b = bytearray(5)
+        nread1 = raw.readinto(b)
+        assert nread1 == 5
 
 
 if __name__ == "__main__":
