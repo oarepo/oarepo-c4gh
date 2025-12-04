ec88894a0c8c9d1a4d8f712df8fbca32a6f90e06
Dominik Pantůček <dominik.pantucek@trustica.cz>
2025-12-04 14:49:38 +0100

    Implement core edit list support.
    - when skipping, discard the data
    - when not skipping, copy it to the buffer
    - in both cases, adjust the head of the edit list

--- oarepo_c4gh/crypt4gh/rawio.py
+++ oarepo_c4gh/crypt4gh/rawio.py
@@ -45,28 +45,67 @@ class Crypt4GHRawIO(RawIOBase):
             return 0
         if self._data_blocks is None:
             self._data_blocks = self._container.data_blocks
+            self._edit_list = self._container.header.edit_list.copy()
+            self._edit_skipping = True
         blen = len(b)
         bpos = 0
         while bpos < blen:
+            # Load next block if the current one is completely used.
             if self._current_block is None or self._current_pos >= len(
                 self._current_block
             ):
                 try:
                     nxt = next(self._data_blocks)
                 except StopIteration:
+                    # Cannot read more blocks, end immediately.
                     self._finished = True
                     return bpos
                 self._current_pos = 0
                 if not nxt.is_deciphered:
+                    # The error raised is similar to failed read from
+                    # a disk which resembles this situation the most.
                     raise OSError
                 self._current_block = nxt.cleartext
-            avail = len(self._current_block) - self._current_pos
-            to_copy = min(blen - bpos, avail)
-            b[bpos : bpos + to_copy] = self._current_block[
-                self._current_pos : self._current_pos + to_copy
-            ]
-            self._current_pos = self._current_pos + to_copy
-            bpos = bpos + to_copy
+            # Check for skipping first
+            if self._edit_skipping and (len(self._edit_list) > 0):
+                # How much is left for skipping and how much data in
+                # the current block remains so that it can be skipped
+                # immediately
+                skip_req = self._edit_list[0]
+                avail = len(self._current_block) - self._current_pos
+                to_skip = min(skip_req, avail)
+                self._edit_list[0] = self._edit_list[0] - to_skip
+                self._current_pos = self._current_pos + to_skip
+                if self._edit_list[0] == 0:
+                    # Skipped enough, flip the flag, advance to next
+                    # count and continue
+                    self._edit_list = self._edit_list[1:]
+                    self._edit_skipping = False
+            else:
+                # Get how much can be copied first.
+                avail = len(self._current_block) - self._current_pos
+                to_copy = min(blen - bpos, avail)
+                # Check whether there are still some edit list counts
+                # left and apply it if necessary.
+                if len(self._edit_list) > 0:
+                    # Adjust to_copy to maximum
+                    max_copy = self._edit_list[0]
+                    to_copy = min(to_copy, max_copy)
+                b[bpos : bpos + to_copy] = self._current_block[
+                    self._current_pos : self._current_pos + to_copy
+                ]
+                self._current_pos = self._current_pos + to_copy
+                bpos = bpos + to_copy
+                if len(self._edit_list) > 0:
+                    # Reduce current not-skipping counter
+                    self._edit_list[0] = self._edit_list[0] - to_copy
+                    if self._edit_list[0] == 0:
+                        # Finished the not-skipping part
+                        self._edit_list = self._edit_list[1:]
+                        if len(self._edit_list) > 0:
+                            # Something left, therefore set the
+                            # skipping flag again.
+                            self._edit_skipping = True
         return bpos
 
     def writable(self) -> bool:
